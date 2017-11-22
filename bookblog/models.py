03fde684b266e9ea7de9c24c8be7d2e77742e698
Samantha Causey <causeys01@gmail.com>
2017-11-22 11:15:16 -0500

    Set up Django, models.py

---
+++ bookblog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    date_published = models.DateTimeField(blank=True, null=True)
+    text = models.TextField()
+
+    def publish(self):
+        self.date_published = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
